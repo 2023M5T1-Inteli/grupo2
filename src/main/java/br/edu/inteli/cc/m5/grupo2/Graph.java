d08e7e4d43ffe2bad12acc8e9e30ee7e6d9c11a4
Rafael-Cabral <99192195+Rafael-Cabral@users.noreply.github.com>
2023-02-24 11:28:56 -0300

    feat: método para encontrar um caminho
    Implementação do método e suas variáveis.

--- src/main/java/br/edu/inteli/cc/m5/grupo2/Graph.java
+++ src/main/java/br/edu/inteli/cc/m5/grupo2/Graph.java
@@ -30,24 +30,24 @@ public class Graph {
         return this.vertices.get(vertexId).getAllConnections();
     }
 
-     public void connectVertices(int distance) {
+    public void connectVertices(int distance) {
 
         int x = (int) (this.vertices.get(this.vertices.size() - 1).getLatitude() - this.vertices.get(0).getLatitude());
-        x = x/distance;
+        x = x / distance;
 
         int y = (int) (this.vertices.get(this.vertices.size() - 1).getLongitude() - this.vertices.get(0).getLongitude());
-        y = y/distance;
+        y = y / distance;
 
         int currentVertex = 0;
         for (int i = 0; i <= x; i++) {
             for (int j = 0; j <= y; j++) {
-                if (i > 0 && j > 0){
+                if (i > 0 && j > 0) {
                     this.addEdge(currentVertex, currentVertex - x - 2);
                 }
                 if (i > 0) {
                     this.addEdge(currentVertex, currentVertex - x - 1);
                 }
-                if (i > 0 && j < y){
+                if (i > 0 && j < y) {
                     this.addEdge(currentVertex, currentVertex - x);
                 }
                 if (j > 0) {
@@ -56,17 +56,30 @@ public class Graph {
                 if (j < y) {
                     this.addEdge(currentVertex, currentVertex + 1);
                 }
-                if (i < x && j > 0){
+                if (i < x && j > 0) {
                     this.addEdge(currentVertex, currentVertex + x);
                 }
                 if (i < x) {
                     this.addEdge(currentVertex, currentVertex + x + 1);
                 }
-                if (i < x && j < y){
+                if (i < x && j < y) {
                     this.addEdge(currentVertex, currentVertex + x + 2);
                 }
-                currentVertex ++;
+                currentVertex++;
             }
         }
     }
-}
+
+    public Vertex[] findPath(Vertex initial, Vertex arrival) {
+
+        double Xsteps = (arrival.getLongitude() - initial.getLongitude()) / 120;
+        double Ysteps = (arrival.getLatitude() - initial.getLatitude()) / 120;
+        Xsteps = Math.round(Xsteps);
+        Ysteps = Math.round(Ysteps);
+        Vertex currentVertex = initial;
+        Vertex[] resultPath = new Vertex[(int) (Xsteps + Ysteps + 1)];
+        resultPath[0] = initial; // o primeiro vértice é o inicial
+        int i = 1; // começa a partir do segundo vértice
+
+
+    }
