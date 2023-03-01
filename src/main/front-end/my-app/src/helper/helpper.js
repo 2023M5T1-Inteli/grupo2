cdabcb5cd0fe0f51c0025aecac6a657be3cf8686
ViniciusSouza25 <99210113+ViniciusSouza25@users.noreply.github.com>
2023-03-01 17:08:12 -0300

    mock de dados
    mock

---
+++ src/main/front-end/my-app/src/helper/helpper.js
@@ -0,0 +1,13 @@
+let coordinates = [];
+
+for(let i = 0; i < 518399; i++) {
+
+    coordinates.push({
+
+        altitude: parseInt(Math.random() * 10000),
+        longitude: Math.random() * 180,
+        latitude: Math.random() * 90
+        
+    })
+    
+}
