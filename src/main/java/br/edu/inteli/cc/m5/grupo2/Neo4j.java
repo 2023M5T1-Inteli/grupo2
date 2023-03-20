7abaf507122d296424471bac35541f7d613cd17f
ViniciusSouza25 <99210113+ViniciusSouza25@users.noreply.github.com>
2023-03-20 16:32:39 -0300

    updateneo4j.java-comments

--- src/main/java/br/edu/inteli/cc/m5/grupo2/Neo4j.java
+++ src/main/java/br/edu/inteli/cc/m5/grupo2/Neo4j.java
@@ -1,3 +1,4 @@
+// Importing required libraries
 package br.edu.inteli.cc.m5.grupo2;
 
 import ch.qos.logback.classic.Logger;
@@ -12,234 +13,101 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+// Defining Neo4j class which implements AutoCloseable
 public class Neo4j implements AutoCloseable {
-    private final Driver driver;
+private final Driver driver;
 
-    public Neo4j() {
+// Constructor that sets up the connection with the Neo4j database
+public Neo4j() {
 
-        String uri = "neo4j://localhost:7687";
-        String user = "neo4j";
-        String password = "x5aG^RSZz!zetdlM19XsJEsa227GsC32";
+    // Declaring URI, user and password to connect to the database
+    String uri = "neo4j://localhost:7687";
+    String user = "neo4j";
+    String password = "x5aG^RSZz!zetdlM19XsJEsa227GsC32";
 
-        LoggerContext loggerContext = (LoggerContext)LoggerFactory.getILoggerFactory();
-        Logger rootLogger = loggerContext.getLogger("io.netty");
-        rootLogger.setLevel(ch.qos.logback.classic.Level.OFF);
+    // Disabling log output for Netty (Neo4j driver dependency) to avoid excessive log prints
+    LoggerContext loggerContext = (LoggerContext)LoggerFactory.getILoggerFactory();
+    Logger rootLogger = loggerContext.getLogger("io.netty");
+    rootLogger.setLevel(ch.qos.logback.classic.Level.OFF);
 
+    // Establishing a connection to the Neo4j database
+    this.driver = GraphDatabase.driver(
+            uri,
+            AuthTokens.basic(user, password),
+            Config.defaultConfig());
 
-        this.driver = GraphDatabase.driver(
-                uri,
-                AuthTokens.basic(user, password),
-                Config.defaultConfig());
+}
 
-    }
+// Method to create vertices in the Neo4j database from a Graph object
+public void createVertices(Graph graph) throws FileNotFoundException {
 
-    public void createVertices(Graph graph) throws FileNotFoundException {
+    StringBuilder queryStr = new StringBuilder();
 
-        StringBuilder queryStr = new StringBuilder();
+    // Iterating over the vertices in the graph and appending the query string to create each vertex
+    graph.getVertices().forEach(vertex -> {
 
-        graph.getVertices().forEach(vertex -> {
+        queryStr.append(String.format(Locale.US, "CREATE (:Vertex { id: %d, latitude: %.5f, longitude: %.5f, altitude: %.5f })\n",
+                vertex.getId(),
+                vertex.getLatitude(),
+                vertex.getLongitude(),
+                vertex.getAltitude()));
 
-            queryStr.append(String.format(Locale.US, "CREATE (:Vertex { id: %d, latitude: %.5f, longitude: %.5f, altitude: %.5f })\n",
-                    vertex.getId(),
-                    vertex.getLatitude(),
-                    vertex.getLongitude(),
-                    vertex.getAltitude()));
+    });
 
-        });
+    // Creating a new query object with the constructed query string
+    Query query = new Query(queryStr.toString());
 
-        Query query = new Query(queryStr.toString());
+    try {
 
-        try {
+        // Executing the query using a write session
+        Session session = driver.session(SessionConfig.forDatabase("neo4j"));
 
-            Session session = driver.session(SessionConfig.forDatabase("neo4j"));
+        Record record = session.executeWrite(tx -> tx.run(query).single());
 
-            Record record = session.executeWrite(tx -> tx.run(query).single());
+        // Printing a success message if the query executed without any errors
+        System.out.println("Graph persisted successfully.");
 
-            System.out.println("Graph persisted successfully.");
+    } catch (Neo4jException exception) {
 
-        } catch (Neo4jException exception) {
-
-            throw exception;
-
-        }
-    }
-
-    public void createVertex(Vertex vertex) {
-
-        Query query = new Query(
-                """
-                        CREATE (vertex:Vertex {
-                                id: $id,
-                                latitude: $latitude,
-                                longitude: $longitude,
-                                altitude: $altitude,
-                                connections: $connections
-                            })
-                            
-                        RETURN vertex
-                        """,
-                Map.of(
-                        "id",vertex.getId(),
-                        "latitude", vertex.getLatitude(),
-                        "longitude", vertex.getLongitude(),
-                        "altitude", vertex.getAltitude(),
-                        "connections", vertex.getAllConnections()));
-
-        try {
-
-            Session session = driver.session(SessionConfig.forDatabase("neo4j"));
-
-            Record record = session.executeWrite(tx -> tx.run(query).single());
-
-        } catch (Neo4jException exception) {
-
-            throw exception;
-
-        }
+        // Rethrowing the exception in case of any errors
+        throw exception;
 
     }
+}
 
-    public void findVertex(int vertexId) {
+// Method to create a single vertex in the Neo4j database
+public void createVertex(Vertex vertex) {
 
-        Query query = new Query(
-                """
-                        MATCH (vertex:Vertex)
-                        WHERE vertex.id = $id
-                        RETURN vertex
-                        """,
-
-                Map.of("id", vertexId));
-
-        try {
-
-            Session session = driver.session(SessionConfig.forDatabase("neo4j"));
-
-            Record record = session.executeWrite(tx -> tx.run(query).single());
-
-            System.out.println("Vertex found successfully: ");
-
-            System.out.println("id: " + record.get("id").asString());
-            System.out.println("latitude: " + record.get("latitude").asString());
-            System.out.println("longitude: " + record.get("longitude").asString());
-            System.out.println("altitude: " + record.get("altitude").asString());
-
-        } catch (Neo4jException exception) {
-
-            throw exception;
-
-        }
-
-    }
-
-    public void connectVertex(Vertex vertexA) {
-
-        vertexA.getAllConnections().forEach(connection -> {
-
-            Vertex vertexB = connection.getArrivalVertex();
-
-            Query query = new Query(
-                    """
-                            MERGE (vertexA:Vertex {
-                                    id: $vertexA_id,
-                                    latitude: $vertexA_latitude,
-                                    longitude: $vertexA_longitude,
-                                    altitude: $vertexA_altitude
-                                })
-                                
-                            MERGE (vertexB:Vertex {
-                                    id: $vertexB_id,
-                                    latitude: $vertexB_latitude,
-                                    longitude: $vertexB_longitude,
-                                    altitude: $vertexB_altitude
-                                })
-                                
-                            MERGE (vertexA)-[:CONNECT_TO]->(vertexB)
-                            
-                            RETURN vertexA, vertexB
-                            """,
-                    Map.of(
-                            "vertexA_id", vertexA.getId(),
-                            "vertexA_latitude", vertexA.getLatitude(),
-                            "vertexA_longitude", vertexA.getLongitude(),
-                            "vertexA_altitude", vertexA.getAltitude(),
-                            "vertexB_id", vertexB.getId(),
-                            "vertexB_latitude", vertexB.getLatitude(),
-                            "vertexB_longitude", vertexB.getLongitude(),
-                            "vertexB_altitude", vertexB.getAltitude()));
-
-            try {
-
-                Session session = driver.session(SessionConfig.forDatabase("neo4j"));
-
-                List records = session.executeWrite(tx -> tx.run(query).list());
-
-            } catch (Neo4jException exception) {
-
-                throw exception;
-
-            }
-
-        });
-
-    }
-
-    public void deleteVertex(Vertex vertex) {
-
-        Query query = new Query(
-                """
-                        MATCH (vertex:Vertex {
-                                id: $id
-                            })
-                            
-                        DETACH DELETE vertex
-                        """,
-                Map.of("id", vertex.getId()));
-
-        try {
-
-            Session session = driver.session(SessionConfig.forDatabase("neo4j"));
-
-            Record record = session.executeWrite(tx -> tx.run(query).single());
-
-            System.out.println("Vertex deleted successfully.");
-
-        } catch (Neo4jException exception) {
-
-            throw exception;
-
-        }
-
-    }
-
-    public void updateVertex(int vertexId, Vertex updatedVertex) {
-
-        Query query = new Query(
-                """
-                        MATCH (vertex:Vertex {
-                                id: $id,
-                            })
+    // Creating a new query object to create a vertex with the provided information
+    Query query = new Query(
+            """
+                    CREATE (vertex:Vertex {
+                            id: $id,
+                            latitude: $latitude,
+                            longitude: $longitude,
+                            altitude: $altitude,
+                            connections: $connections
+                        })
                         
-                        SET vertex.latitude = $latitude,
-                            vertex.longitude = $longitude,
-                            vertex.altitude: $altitude
-                            
-                        RETURN vertex
-                        """,
-                Map.of(
-                        "id", vertexId,
-                        "latitude", updatedVertex.getLatitude(),
-                        "longitude", updatedVertex.getLongitude(),
-                        "altitude", updatedVertex.getAltitude()));
+                    RETURN vertex
+                    """,
+            Map.of(
+                    "id",vertex.getId(),
+                    "latitude", vertex.getLatitude(),
+                    "longitude", vertex.getLongitude(),
+                    "altitude", vertex.getAltitude(),
+                    "connections", vertex.getAllConnections()));
 
-        try {
+    try {
 
-            Session session = driver.session(SessionConfig.forDatabase("neo4j"));
+        // Executing the query using a write session
+        Session session = driver.session(SessionConfig.forDatabase("neo4j"));
 
-            Record record = session.executeWrite(tx -> tx.run(query).single());
+        Record record = session.executeWrite(tx -> tx.run(query).single());
 
-            System.out.println("Vertex updated successfully.");
+    } catch (Neo4jException exception) {
 
-        } catch (Neo4jException exception) {
+        // Rethrowing the exception in case of any errors
 
             throw exception;
 
